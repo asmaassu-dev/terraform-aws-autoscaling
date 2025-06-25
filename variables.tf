03c1215a9f2c7b01affe9cb60ddf64daf5efced7
asmaassu-dev <asmaassu0123@gmail.com>
2025-06-25 12:40:09 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID to use for EC2"
+  default     = "ami-0c55b159cbfafe1f0"
+}
